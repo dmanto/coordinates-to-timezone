092a958adbc096852127049dcc6fd6607fd542c4
Daniel Mantovani <dmanto@gmail.com>
2021-10-08 09:10:55 -0300

    working version

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+'use strict';
+
 const express = require('express');
 const geoTz = require('geo-tz');
-const app = express();
 
 const port = 8080;
+const host = '0.0.0.0';
 
+const app = express();
 app.get('/tz.json', (req, res) => {
     res.json(geoTz(req.query.lat, req.query.long));
 });
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}!`);
+    console.log(`App listening on http://${host}:${port}!`);
 });
